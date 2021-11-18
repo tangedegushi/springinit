7c5c111985d4ab57397d0ff0a3cc68f1a86df120
ZouZhiQiang <zhiqiang.zou@ubtrobot.com>
2021-11-18 15:49:13 +0800

    feat:查询语句改为用注解的方式，弃用在xml中配置

--- src/main/java/com/springboot/springinit/SpringinitApplication.java
+++ src/main/java/com/springboot/springinit/SpringinitApplication.java
@@ -1,9 +1,11 @@
 package com.springboot.springinit;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({"com.springboot.springinit.dao"})
 public class SpringinitApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/springboot/springinit/dao/StudentDao.java
+++ src/main/java/com/springboot/springinit/dao/StudentDao.java
@@ -2,12 +2,13 @@ package com.springboot.springinit.dao;
 
 import com.springboot.springinit.entity.Student;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
-@Mapper
 @Repository
 public interface StudentDao {
 
+    @Select("SELECT * FROM my_student WHERE id = #{id}")
     Student selectStudentById(int id);
 
 }
